92aa9dbac6c2def66e17ab923d17804f366bd4d0
Marvin MATIME <Marvel2002@hotmail.fr>
2017-05-02 09:42:30 +0200

    lib

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mmatime <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 13:37:10 by mmatime           #+#    #+#             */
-/*   Updated: 2017/05/02 09:39:28 by mmatime          ###   ########.fr       */
+/*   Updated: 2017/05/02 09:42:14 by mmatime          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ void			ft_putendl(char const *s);
 void			ft_putnbr(int n);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putstrtab(char **str);
-void			ft_putinttab(int **tab);
+void			ft_putinttab(int **tab, int x, int y);
 void			ft_putstr_fd(char const *s, int fd);
 void			ft_putendl_fd(char const *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
