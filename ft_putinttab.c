dbc822a5326f1973b88b96152263d146cd6e19b2
Marvin MATIME <Marvel2002@hotmail.fr>
2017-10-09 12:05:05 +0200

    tabint

--- ft_putinttab.c
+++ ft_putinttab.c
@@ -6,22 +6,22 @@
 /*   By: mmatime <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/02 09:17:19 by mmatime           #+#    #+#             */
-/*   Updated: 2017/05/02 09:44:49 by mmatime          ###   ########.fr       */
+/*   Updated: 2017/10/09 12:04:51 by mmatime          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putinttab(int **tab, int x, int y)
+void	ft_putinttab(int **tab, int width, int height)
 {
 	int i;
 	int j;
 
 	i = 0;
-	while (i < x)
+	while (i < width)
 	{
 		j = 0;
-		while (j < y)
+		while (j < height)
 		{
 			ft_putnbr(tab[i][j]);
 			ft_putchar(' ');
