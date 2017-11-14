ff1d2645262730ccd4c8de32c2fdbfdb6fa4432e
Marvin MATIME <Marvel2002@hotmail.fr>
2017-11-14 14:11:14 +0100

    lib

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: mmatime <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/16 17:36:20 by mmatime           #+#    #+#             */
-/*   Updated: 2017/11/14 14:07:25 by mmatime          ###   ########.fr       */
+/*   Updated: 2017/11/14 14:09:50 by mmatime          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		str = ft_strdup(s2);
 		return (str);
 	}
-	str = (char *)malloc(sizeof(char) * ((ft_strlen(i) + ft_strlen(j)) + 1));
+	str = (char *)malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
 	if (!str)
 		return (NULL);
 	i = 0;
